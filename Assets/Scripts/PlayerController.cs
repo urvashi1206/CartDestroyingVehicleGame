4932b5f5891aa120b7280a9aa57b8b5e2d3f1cb2
urvashi1206 <dhingraurvashi99@gmail.com>
2024-05-19 20:54:01 -0400

    added the center of mass and some UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
+    [SerializeField] private float speed;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI SpeedText;
+    [SerializeField] TextMeshProUGUI RPMText;
+    [SerializeField] float rpm;
+    [SerializeField] GameObject[] allwheels;
+    [SerializeField] int wheelsOnGround;
+
+    private Rigidbody rb;
+    [SerializeField] private float HorsePower = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        if (isOnGround())
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+            speed = Mathf.RoundToInt(rb.velocity.magnitude * 2.237f);
+            SpeedText.text = "Speed: " + speed + " mph";
+
+            rpm = (speed % 30) * 40;
+            RPMText.text = "RPM: " + rpm;
+
+            rb.AddRelativeForce(Vector3.forward * forwardInput * HorsePower);
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
+    }
+
+    bool isOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (GameObject wheel in allwheels)
+        {
+            if (wheel.GetComponent<WheelCollider>().isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
